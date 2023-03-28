cb437726ae8bfe8794ce3d4533c7e712779c2937
DarkLord <dipeshrathore122@gmail.com>
2023-03-28 14:21:21 +0530

    Introduction - HelloWorld

---
+++ Java + DSA/Intro to Java - Lec 8/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld{
+    public static void main(String[] args) { // String[] args is Array of Command Line Arguments given at the runtime of .class file.
+        System.out.println("Hello World, Welcome in the World of Java!!");
+    }
+}
+
+
