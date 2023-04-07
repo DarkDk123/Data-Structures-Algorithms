01afe8c9e059d6e3d00079a211b48950cd724ed5
DarkLord <dipeshrathore122@gmail.com>
2023-04-07 14:14:35 +0530

    Binary Search using While Loop

---
+++ Java + DSA/Lec 14 - Binary search ALgo/BinarySearch.java
@@ -0,0 +1,37 @@
+/**
+ * BinarySearch
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        // Main Function
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int key = 3;
+        int res = SearchBinary(key, array);
+        System.out.println((res != -1) ? "** Found at index " + res : "** Not Found!!");
+    }
+
+    static int SearchBinary(int key, int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+
+        // Edge Case required for saving one unnecessary Iteration of while loop.
+        if (array.length == 0) {
+            return -1;
+        }
+
+        while (start <= end) {
+            /*
+             * Using this cause same mathematical answer but calculates mid by substraction
+             * instead addition ((s+e)/2) to reduce the risk of int overflow.
+             */
+            int mid = start + (end-start)/2;//(start + end) / 2;
+            if (key == array[mid])
+                return mid;
+            else if (key > array[mid])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+}
