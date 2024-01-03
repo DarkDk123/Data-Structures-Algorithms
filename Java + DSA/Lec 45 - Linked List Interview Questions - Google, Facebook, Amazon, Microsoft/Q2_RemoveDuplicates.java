ab301bf74b01029d2c25c118f2a74917a35ddc6e
DarkLord <dipeshrathore122@gmail.com>
2024-01-03 16:48:04 +0530

    Q. Remove duplicates from a sorted Linked List.

---
+++ Java + DSA/Lec 45 - Linked List Interview Questions - Google, Facebook, Amazon, Microsoft/Q2_RemoveDuplicates.java
@@ -0,0 +1,30 @@
+/**
+ * Q2 A Leet-code question to remove duplicates from a sorted LinkedList.
+ * 
+ * Here's the Link : "https://leetcode.com/problems/remove-duplicates-from-sorted-list/"
+ */
+
+public class Q2_RemoveDuplicates {
+
+    // Definition for singly-linked list.
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+     
+    // Deleting duplicate Nodes from a Sorted Linked List.
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+            if (temp.val == temp.next.val)
+                temp.next = temp.next.next;
+            else
+                temp = temp.next;
+        }
+
+        return head;
+    }
+}
